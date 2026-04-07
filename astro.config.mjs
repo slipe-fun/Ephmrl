da251e8e27550e6231fa60aa7f482a545eb82c46
Dikiy Perec <hopabsq@mail.ru>
2026-04-08 03:35:51 +0600

    Add template for future improvments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import svelte from '@astrojs/svelte';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+
+  vite: {
+    // @ts-ignore
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+        "@icons": path.resolve("./src/assets/icons"),
+        "@lib": path.resolve("./src/lib"),
+      },
+    },
+  }
 });
---
+++ src/lib/icons/index.ts
@@ -0,0 +1,27 @@
+import ArrowRight from "@icons/arrow.right.svg";
+import Checkbox from "@icons/checkbox.svg";
+import Code from "@icons/code.svg";
+import Grid from "@icons/grid.svg";
+import Image from "@icons/image.svg";
+import Link from "@icons/link.svg";
+import List from "@icons/list.svg";
+import Message from "@icons/message.svg";
+import Paperplane from "@icons/paperplane.svg";
+import Table from "@icons/table.svg";
+import Text from "@icons/text.svg";
+import Title from "@icons/title.svg";
+
+export const Icons = {
+  arrowRight: ArrowRight,
+  checkbox: Checkbox,
+  code: Code,
+  grid: Grid,
+  image: Image,
+  link: Link,
+  list: List,
+  message: Message,
+  paperplane: Paperplane,
+  table: Table,
+  text: Text,
+  title: Title,
+};
